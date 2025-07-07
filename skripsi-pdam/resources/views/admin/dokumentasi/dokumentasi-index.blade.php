b583d0e312ebd2961d846bbbb5035ad2340ea5ee
Dimas Pamungkas Mursidi <dmspmksmrsdi@gmail.com>
2025-07-07 15:12:40 +0700

    Update

--- skripsi-pdam/resources/views/admin/dokumentasi/dokumentasi-index.blade.php
+++ skripsi-pdam/resources/views/admin/dokumentasi/dokumentasi-index.blade.php
@@ -40,9 +40,15 @@
             @endforeach
         </div>
         @else
-        <div class="text-center py-20">
-            <p class="text-gray-500 text-lg">Belum ada dokumentasi teknisi.</p>
-        </div>
+            <div class="col-span-full flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
+                <div class="w-32 mb-4 text-gray-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 88 88" fill="currentColor">
+                        <path d="m86.69 32.608-8.65-4.868 8.65-4.868a1 1 0 0 0 0-1.744l-32-18a1.002 1.002 0 0 0-.98 0L44 8.593l-9.71-5.465a1.002 1.002 0 0 0-.98 0l-32 18a1 1 0 0 0 0 1.744l8.65 4.868-8.65 4.868a1 1 0 0 0 0 1.744l9.69 5.45V66a1.001 1.001 0 0 0 .51.872l32 18A1.203 1.203 0 0 0 44 85a1.232 1.232 0 0 0 .49-.128l32-18A1.001 1.001 0 0 0 77 66V39.802l9.69-5.45a1 1 0 0 0 0-1.744zM43 44.03 14.04 27.74 43 11.45zm2-32.58 28.96 16.29L45 44.03zm9.2-6.303L84.161 22 76 26.593 46.04 9.74zm-20.4 0 8.16 4.593-22.47 12.64L12 26.593 3.839 22zM12 28.887 41.96 45.74l-8.16 4.593L3.839 33.48zm1 12.042 20.31 11.423a1 1 0 0 0 .98 0L43 47.45v34.84L13 65.415zm62 0v24.486L45 82.29V47.45l8.71 4.901a1 1 0 0 0 .98 0zm-20.8 9.404-8.16-4.593L76 28.888l8.161 4.592z" />
+                    </svg>
+                </div>
+                <h1 class="text-xl font-semibold text-gray-700">Tidak ada Data Dokumentasi</h1>
+                <!-- <p class="text-sm text-gray-500 mt-2">Teknisi belum membuat dokumentasi.</p> -->
+            </div>
         @endif
     </div>
 </x-home>
