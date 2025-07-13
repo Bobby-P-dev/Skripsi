fed3c4ea544b7d918c146f850d355ca57b2e7f04
Bobby Pratama <bobay772@gmail.com>
2025-07-13 21:58:58 +0700

    solve problem

--- skripsi-pdam/app/Providers/AppServiceProvider.php
+++ skripsi-pdam/app/Providers/AppServiceProvider.php
@@ -44,8 +44,8 @@ class AppServiceProvider extends ServiceProvider
                     $platform->registerDoctrineTypeMapping('enum', 'string');
                 }
             } catch (\Throwable $e) {
-                // Abaikan error koneksi database saat startup agar aplikasi tidak crash.
-                // Fungsionalitas enum akan berjalan saat koneksi normal.
+                // Abaikan error ini saat startup agar aplikasi tidak crash
+                report($e);
             }
         }
     }
