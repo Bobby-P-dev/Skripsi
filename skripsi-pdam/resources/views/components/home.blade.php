13ed741bf616469a86067d215acf8fea96bdde02
Dimas Pamungkas Mursidi <dmspmksmrsdi@gmail.com>
2025-07-13 19:46:19 +0700

    Update favicon

--- skripsi-pdam/resources/views/components/home.blade.php
+++ skripsi-pdam/resources/views/components/home.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/images/tirta.png">
     <title>PDAM Dashboard</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
